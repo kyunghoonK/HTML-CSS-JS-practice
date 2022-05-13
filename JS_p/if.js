9aa82f1fde27142530a8470c6d8010f651f57450
kyunghoon <rlarudgns970@naver.com>
2022-05-13 15:23:10 +0900

    .

---
+++ JS_p/if.js
@@ -0,0 +1,12 @@
+let juYoung = 24
+
+if(juYoung == 24){
+  console.log("주영이의 나이가 맞습니다.")
+}else{
+  console.log("주영이의 나이가 아닙니다.")
+}
+
+//삼항연산식: 조건? true일때 : false일때
+let answer = juYoung == 24? "주영이의 나이가 맞습니다." : "주영이의 나이가 아닙니다."
+
+console.log(answer)
